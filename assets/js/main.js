dea30372fc7893d9d8e4087d87cf560c141d167c
Egor Yurtaev <yurtaev.egor@gmail.com>
2015-08-16 00:20:53 +0600

    update cats

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@ angular.module('app').controller('AppCtrl', function ($scope) {
   console.log('AppCtrl');
   $scope.photos = [
     {title: 'photo #1', href: 'http://lorempixel.com/1240/800/cats/1/'},
-    {title: 'photo #2', href: 'http://lorempixel.com/1240/800/cats/2/'},
-    {title: 'photo #3', href: 'http://lorempixel.com/1240/800/cats/3/'},
-    {title: 'photo #4', href: 'http://lorempixel.com/1240/800/cats/4/'}
+    {title: 'photo #2', href: 'http://lorempixel.com/1240/800/cats/5/'},
+    {title: 'photo #3', href: 'http://lorempixel.com/1240/800/cats/10/'},
+    {title: 'photo #4', href: 'http://lorempixel.com/1240/800/cats/7/'}
   ];
 });
