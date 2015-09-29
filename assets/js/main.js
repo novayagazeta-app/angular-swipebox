313b64be4a19d7185267878d0d50bb3cb576a5df
Zhulduz Zhankenova <zhankenova@gmail.com>
2015-09-30 00:04:13 +0600

    use version 0.0.11

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ angular.module('app', ['ngSwipebox']);
 angular.module('app').controller('AppCtrl', function ($scope) {
   console.log('AppCtrl');
   $scope.photos = [
-    {title: 'photo #1', href: 'assets/img/1.jpeg'},
+    {title: 'photo #1', href: 'assets/img/1.jpeg', src:'assets/img/5.jpg'},
     {title: 'photo #2', href: 'assets/img/2.jpeg'},
     {title: 'photo #3', href: 'assets/img/3.jpeg'},
     {title: 'photo #4', href: 'assets/img/4.jpeg'}
